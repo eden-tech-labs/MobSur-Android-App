e79904e30d0aede1a16dd0b9f7f5c0215f1346b6
nelietl <115985447+nelietl@users.noreply.github.com>
2022-10-25 18:12:48 +0300

    refactor: updated project with the new Android SDK

--- app/src/main/java/com/example/mobsurandroiddemo/MainActivity.kt
+++ app/src/main/java/com/example/mobsurandroiddemo/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MobSur.setup(applicationContext, "36121162-25d6-40f1-ab24-d5bd87a47601", "userId")
+        MobSur.setup(applicationContext, "36121162-25d6-40f1-ab24-d5bd87a47601", "userId_1")
         MobSur.setFragmentManager(supportFragmentManager)
 
         val button = findViewById(R.id.button) as Button
