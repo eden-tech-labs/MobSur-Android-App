35db59eae8e0d5f147cab494d976c2eb294a94ed
Lachezar Todorov <lachezar.todorov@edentechlabs.io>
2022-11-14 15:39:07 +0200

    Updated SDK and added random user id

--- app/src/main/java/com/example/mobsurandroiddemo/MainActivity.kt
+++ app/src/main/java/com/example/mobsurandroiddemo/MainActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import io.edentechlabs.survey.sdk.MobSur
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MobSur.setup(applicationContext, "36121162-25d6-40f1-ab24-d5bd87a47601", "userId_1")
+        val userId = Random.nextInt(from = 0, until = Int.MAX_VALUE)
+        MobSur.setup(applicationContext, "36121162-25d6-40f1-ab24-d5bd87a47601", "$userId", debug = true)
         MobSur.setFragmentManager(supportFragmentManager)
 
         val button = findViewById(R.id.button) as Button
